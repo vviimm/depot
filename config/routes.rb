89938db3b5e98fa5da8b42600dda3114cb001a40
vviimm <wowaonrails@gmail.com>
2016-01-12 17:07:57 +0300

    Testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  root 'store#index'
   resources :products
 end
---
+++ test/controllers/products_controller_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+class ProductsControllerTest < ActionController::TestCase
+
+    setup do
+        @product = products(:one)
+
+            @update = {
+                title: 'Lorem Ipsum',
+                description: 'Wibbles are fun!',
+                image_url: 'lorem.jpg',
+                price: 19.95
+            }
+    end
+
+    test "should get index" do
+        get :index
+        assert_response :success
+        assert_not_nil assigns(:products)
+    end
+
+    test "should get new" do
+        get :new
+        assert_response :success
+    end
+
+    test "should create product" do
+            assert_difference('Product.count') do
+            post :create, product: @update
+        end
+        assert_redirected_to product_path(assigns(:product))
+    end
+
+    test "should update product" do
+            patch :update, id: @product, product: @update
+            assert_redirected_to product_path(assigns(:product))
+    end
+
+    test "should get products index" do
+        get :index
+        assert_response :success
+        assert_select '.products .list_description', minimum: 3
+        assert_select '.products .list_description dl dt', 'Programming Ruby 1.9'
+        assert_select '.products .list_description dl dd', /.{1,80}/
+        assert_select '.products .list_actions', minimum: 3
+        ['Show', 'Edit', 'Destroy'].each do |action|
+          assert_select '.products .list_actions a', action
+        end
+        assert_select 'a', 'New Product'
+    end
+
+end
