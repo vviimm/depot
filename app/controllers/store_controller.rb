96b544a5e1f72debd8187d7e0a51b5dab5f4a97f
vviimm <wowaonrails@gmail.com>
2016-01-13 15:50:41 +0300

    Added model Cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,27 @@
 class StoreController < ApplicationController
+  include ActionView::Helpers::TextHelper
+
   def index
     @products = Product.order(:title)
+    @count = session_count
+    @session_greeting_msg = session_greeting
+      if @count > 5
+        flash[:notice] = @session_greeting_msg
+      end
   end
+
+  private
+
+    def session_count
+      session[:counter] ||= 0
+      session[:counter] += 1
+    end
+
+    def session_greeting
+      if @count == 0
+        session_greeting = "welcome!"
+      else
+        session_greeting = "You've been here " + pluralize(@count, 'time')
+      end
+    end
 end
